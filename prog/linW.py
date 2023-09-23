16ab8de7967a7b3c045a36c7db0095533c6b51e0
FiaLDI <leon.youtube2017@yandex.ru>
2023-09-23 12:45:13 +0300

    add progs

---
+++ prog/linW.py
@@ -0,0 +1,18 @@
+
+def leniarPoisk(a, b):
+    for i in range(len(a)):
+        if a[i] == b:
+            return i
+    return -1
+
+if __name__ == '__main__':
+    import time
+    for i in range(10, 100, 10):
+        a = [j for j in range(i)]
+        b = 0
+        for o in range(len(a), i + 100):
+            start = time.perf_counter()
+            leniarPoisk(a, o)
+            end = time.perf_counter()
+            b += end - start
+        print(f"{i} {(b / i):.8f}")
